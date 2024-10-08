0e34925a3cbfffc9fa30b11aca7c9654902daae3
Drew <dreydreyq@gmail.com>
2024-10-09 05:29:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("cancelBtn").onclick = function(){
+    window.alert("You have successfully cancel your order");
+}
+
+document.getElementById("submitBtn").onclick = function(){
+    window.alert("We have successfully received your order!");
+    window.alert("Wait for our customer service to call for the payment process, Thank you!");
+}
